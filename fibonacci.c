434d9a8bae5043820dea27284b0f2500ed991643
SWATHI <47173697+SWATHIPRIYA16@users.noreply.github.com>
2019-02-03 16:16:33 +0530

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int i,n,t1=1,t2=1,next;
+    scanf("%d",&n);
+    for (i=1;i<=n;++i)
+    {
+        printf("%d, ", t1);
+        next= t1+t2;
+        t1 = t2;
+        t2 = next;
+    }
+}
